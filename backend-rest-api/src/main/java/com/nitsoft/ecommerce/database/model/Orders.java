d6fb52a177a0dff2679cf72a3319403158d75702
vipul <vsharma.1634@gmail.com>
2021-05-23 01:02:22 +0530

    code refactor

--- backend-rest-api/src/main/java/com/nitsoft/ecommerce/database/model/Orders.java
+++ backend-rest-api/src/main/java/com/nitsoft/ecommerce/database/model/Orders.java
@@ -14,24 +14,16 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.xml.bind.annotation.XmlRootElement;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
+import lombok.*;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
-@AllArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
-@DynamicInsert
-@DynamicUpdate
 @Table(name = "orders")
-@XmlRootElement
 public class Orders implements Serializable {
 
     private static final long serialVersionUID = 1L;
