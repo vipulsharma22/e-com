f185758097056ec2b3a1e9626138e28b28ac4c73
vipul <vsharma.1634@gmail.com>
2021-06-24 11:11:16 +0530

    Cors filter changes

--- backend-rest-api/src/main/java/com/nitsoft/ecommerce/CorsFilter.java
+++ backend-rest-api/src/main/java/com/nitsoft/ecommerce/CorsFilter.java
@@ -5,20 +5,28 @@
  */
 package com.nitsoft.ecommerce;
 
+import com.google.common.collect.Lists;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 
 @Component
 public class CorsFilter implements Filter {
 
+    private static final Set<String> allowedOrigins = new HashSet<String>(Lists.newArrayList("localhost:3000","https://lil-munchkin.herokuapp.com","0:0:0:0:0:0:0:1"));
+
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        String originHeader = req.getRemoteHost();
+        if(allowedOrigins.contains(originHeader)){
+            response.setHeader("Access-Control-Allow-Origin", originHeader);
+        }
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, x-requested-with, X-Custom-Header, X-Access-Token");
